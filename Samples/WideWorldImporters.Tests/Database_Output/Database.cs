1d2a8895d9521afcb66a1c0025bfcacc64cb0750
John Welch <jwelch@sentryone.com>
2019-07-12 12:04:16 -0400

    Updates for package references, adding debug logging

--- Samples/WideWorldImporters.Tests/Database_Output/Database.cs
+++ Samples/WideWorldImporters.Tests/Database_Output/Database.cs
@@ -7,6 +7,11 @@ namespace WideWorldImporters.Tests.Database_Output
     // The content of this file is preserved when the test suite is regenerated.
     public partial class Database
     {
+        static partial void BeforeTest(string testName, Dictionary<string, object> testResources, ref bool cancel)
+        {
+            // Print debug information to the console to aid with parameter debugging.
+            PragmaticWorks.LegiTest.Runtime.ParameterProvider.PrintDebugInformation(parameterProvider);
+        }
     }
 }
 
