705cf5c29da07e34f2b92c3516dd623129cd7987
John Welch <jwelch@sentryone.com>
2019-07-12 14:33:32 -0400

    Script for parameter replacement

---
+++ Samples/RuntimeSettings/TokenReplacement.ps1
@@ -0,0 +1,18 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+        [string]$sourceFile,
+    [Parameter(Mandatory=$false)]
+        [string]$targetFile = $sourceFile,
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+        [string[]]$replacementList 
+ )
+
+$content = (Get-Content $sourceFile)
+foreach($replaceItem in $replacementList)
+{
+    $replace = $replaceItem.Split("|")
+    $content = $content.Replace($replace[0], $replace[1])
+}
+
+Set-Content -Value $content -Path $targetFile
