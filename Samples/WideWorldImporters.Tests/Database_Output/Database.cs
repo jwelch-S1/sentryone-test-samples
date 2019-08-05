a028d90d4f83108ada77d2ed8e25b915ed723943
John Welch <jwelch@sentryone.com>
2019-08-05 13:47:59 -0400

    Removing logging code

--- Samples/WideWorldImporters.Tests/Database_Output/Database.cs
+++ Samples/WideWorldImporters.Tests/Database_Output/Database.cs
@@ -1,17 +1,9 @@
 namespace WideWorldImporters.Tests.Database_Output
 {
-    using System.Collections.Generic;
-    
-    
     // This file provides a point at which partial methods can be implemented to augment tests.
     // The content of this file is preserved when the test suite is regenerated.
     public partial class Database
     {
-        static partial void BeforeTest(string testName, Dictionary<string, object> testResources, ref bool cancel)
-        {
-            // Print debug information to the console to aid with parameter debugging.
-            PragmaticWorks.LegiTest.Runtime.ParameterProvider.PrintDebugInformation(parameterProvider);
-        }
     }
 }
 
